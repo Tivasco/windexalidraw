4a4e9eaab3c600ea4fc985ad2def6ffa8c5773d8
Tiago Vasco <tiago.jvasco@gmail.com>
2025-03-17 23:19:16 +0000

    new version of live colab working

---
+++ fix-lexical-declarations.js
@@ -0,0 +1,1 @@
+console.log("Fixing lexical declaration errors")
--- src/config/keycloak.config.ts
+++
@@ -1,8 +0,0 @@
-// Update these values with your actual Keycloak configuration
-const keycloakConfig = {
-  url: 'https://keycloak.dev-ideasapp.com',
-  realm: 'prompt-com',
-  clientId: 'prompt-frontend'
-};
-
-export default keycloakConfig; 
